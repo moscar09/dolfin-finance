cc09881c51f1db716ed0854ed67cdb6f9bfd8e2e
Oscar Marginean <hello@oscar.sh>
2024-11-09 16:43:28 +0100

    Added pagination

--- apps/client/src/app/components/organisms/TransactionsCard.tsx
+++ apps/client/src/app/components/organisms/TransactionsCard.tsx
@@ -4,7 +4,9 @@ import { DataTable } from 'mantine-datatable';
 import { usePatchTransaction } from '../../hooks/mutations/usePatchTransaction';
 import { useBudgetCategories } from '../../hooks/queries/useBudgetCategories';
 import { MoneyAmount } from '../atoms/MoneyAmount';
+import { useEffect, useState } from 'react';
 
+const PAGE_SIZE = 25;
 export function TransactionsCard({
   isLoading,
   transactions,
@@ -14,11 +16,30 @@ export function TransactionsCard({
 }) {
   const { isPending: budgetCategoriesLoading, data: budgetCategories } =
     useBudgetCategories();
-
   const patchTransaction = usePatchTransaction();
+
+  const [page, setPage] = useState(1);
+  const [records, setRecords] = useState(
+    transactions?.slice(0, PAGE_SIZE) || []
+  );
+
+  useEffect(() => {
+    const from = (page - 1) * PAGE_SIZE;
+    const to = from + PAGE_SIZE;
+    setRecords(transactions?.slice(from, to) || []);
+  }, [page, transactions]);
+
+  useEffect(() => {
+    if (page !== 1) setPage(1);
+  }, [transactions]);
 
   if (!budgetCategoriesLoading && !budgetCategories)
     throw Error('Could not find budget categories');
+
+  const budgetSelectorData = (budgetCategories || []).map((category) => ({
+    value: `${category.id}`,
+    label: category.name,
+  }));
 
   return (
     <DataTable
@@ -29,7 +50,11 @@ export function TransactionsCard({
       striped
       fetching={isLoading || budgetCategoriesLoading}
       highlightOnHover
-      records={transactions}
+      totalRecords={transactions?.length || 0}
+      records={records}
+      recordsPerPage={PAGE_SIZE}
+      page={page}
+      onPageChange={(p) => setPage(p)}
       columns={[
         {
           accessor: 'date',
@@ -65,10 +90,7 @@ export function TransactionsCard({
             <Select
               size="sm"
               placeholder="Pick value"
-              data={(budgetCategories || []).map((category) => ({
-                value: `${category.id}`,
-                label: category.name,
-              }))}
+              data={budgetSelectorData}
               defaultValue={`${budgetCategoryId}`}
               comboboxProps={{ width: 180, position: 'bottom-end' }}
               onChange={(v) =>
